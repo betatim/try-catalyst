36fdd7ebbef66cd0aa2e4c4d00262caf6ff63779
Tim Head <betatim@gmail.com>
2021-05-06 21:43:16 +0200

    Fix classname removal/mangling

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  optimization: {
+    // You could completely disable minimisation by uncommenting the following
+    // line
+    //minimize: false,
+    // or you can set the `keey_classnames` option of the terser plugin to true
+    // This seems more desirable
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+        }
+      })
+    ]
+  },
   mode: "production",
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   entry: "./src/index.ts",
   module: {
     rules: [
